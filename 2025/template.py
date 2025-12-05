aab21d356d8b3d738842a0904bb79c86ddcb2a73
Gilles DEVILLERS <gilles.devillers@sectronic.fr>
2025-12-05 11:35:08 +0100

    🧑‍💻 Add template (very basic)

---
+++ 2025/template.py
@@ -0,0 +1,22 @@
+import os
+
+day = int(os.path.basename(__file__)[3:5])
+
+with open(f"2025/inputs/day{day:02}.txt") as f:
+    raw_input = f.read().split("\n")
+
+# Part 1
+
+total = 0
+
+
+
+print(f"Part 1: {total}")
+
+# Part 2
+
+total = 0
+
+
+
+print(f"Part 2: {total}")
